5b3fa58732f8b7d824de2b81dd403e3778f02c40
Rennan Rezende <rennan765@gmail.com>
2020-02-16 21:02:16 -0300

    CrossCutting configuration.

---
+++ 4oito6/4oito6.Infra.CrossCutting.Configuration.Core/Implementation/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using _4oito6.Infra.CrossCutting.Configuration.Core.Interfaces;
+using System;
+
+namespace _4oito6.Infra.CrossCutting.Configuration.Core.Implementation
+{
+    public abstract class Configuration : IConfiguration
+    {
+        public string DbConnectionString => Environment.GetEnvironmentVariable("DbConnectionString");
+
+        public string CacheConnectionString => Environment.GetEnvironmentVariable("CacheConnectionString");
+    }
+}
---
+++ 4oito6/4oito6.Infra.CrossCutting.Configuration.Core/Interfaces/IConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace _4oito6.Infra.CrossCutting.Configuration.Core.Interfaces
+{
+    public interface IConfiguration
+    {
+        string DbConnectionString { get; }
+        string CacheConnectionString { get; }
+    }
+}
