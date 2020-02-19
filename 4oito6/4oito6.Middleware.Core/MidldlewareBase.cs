f4c031eddebdc53b599307cda607dec51b4986eb
Rennan Rezende <rennan765@gmail.com>
2020-02-19 15:48:00 -0300

    Middleware.

---
+++ 4oito6/4oito6.Middleware.Core/MidldlewareBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace _4oito6.Middleware.Core
+{
+    public abstract class MidldlewareBase
+    {
+        private readonly RequestDelegate _next;
+
+        public MidldlewareBase(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public virtual async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
