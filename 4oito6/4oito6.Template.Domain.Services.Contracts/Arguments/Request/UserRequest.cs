18597abf0eb91a66dd48cbf2ae84aca0a5c41083
Rennan Rezende <rennan765@gmail.com>
2020-03-18 19:55:59 -0300

    Contracts from Services. Implementation proect.

---
+++ 4oito6/4oito6.Template.Domain.Services.Contracts/Arguments/Request/UserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace _4oito6.Template.Domain.Services.Contracts.Arguments.Request
+{
+    public class UserRequest
+    {
+        public int? Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public IList<UserPhoneRequest> Phones { get; set; }
+    }
+}
---
+++ 4oito6/4oito6.Template.Domain.Services.Contracts/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using _4oito6.Domain.Services.Core.Contracts.Interfaces;
+using _4oito6.Template.Domain.Services.Contracts.Arguments.Request;
+using _4oito6.Template.Domain.Services.Contracts.Arguments.Response;
+using System.Threading.Tasks;
+
+namespace _4oito6.Template.Domain.Services.Contracts.Interfaces
+{
+    public interface IUserService : IServiceBase
+    {
+        Task<UserResponse> CreateUser(UserRequest request);
+    }
+}
