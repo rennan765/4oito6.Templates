f8666d23907f1c06c2dfd5d83fda2a31e2c0365e
Rennan Rezende <rennan765@gmail.com>
2020-03-18 19:16:03 -0300

    Entidades.

---
+++ 4oito6/4oito6.Template.Domain.Model/Entities/Address.cs
@@ -0,0 +1,28 @@
+﻿using _4oito6.Domain.Model.Core.Entities;
+
+namespace _4oito6.Template.Domain.Model.Entities
+{
+    public class Address : EntityBase
+    {
+        public long Id { get; private set; }
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string District { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string PostalCode { get; private set; }
+
+        public Address(long id, string street, string number, string complement, string district, string city, string state, string postalCode)
+        {
+            Id = id;
+            Street = street;
+            Number = number;
+            Complement = complement;
+            District = district;
+            City = city;
+            State = state;
+            PostalCode = postalCode;
+        }
+    }
+}
---
+++ 4oito6/4oito6.Template.Domain.Model/Entities/User.cs
@@ -0,0 +1,48 @@
+﻿using _4oito6.Domain.Model.Core.Entities;
+using _4oito6.Template.Domain.Model.ValueObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4oito6.Template.Domain.Model.Entities
+{
+    public class User : EntityBase
+    {
+        public int Id { get; private set; }
+        public Name Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public Address Address { get; private set; }
+
+        public IList<Phone> Phones { get; set; }
+
+        public void ChangeAddress(Address address)
+        {
+            Address = address;
+        }
+
+        public void AddPhone(string localCode, string number)
+        {
+            if (!Phones.Any(p => p.LocalCode == localCode && p.Number == number))
+                Phones.Add(new Phone(localCode, number));
+        }
+
+        public void RemovePhone(string localCode, string number)
+        {
+            var phone = Phones.FirstOrDefault(p => p.LocalCode == localCode && p.Number == number);
+
+            if (phone != null)
+                Phones.Remove(phone);
+        }
+
+        public void ChangePhone(string oldLocalCode, string oldNumber, string newLocalCode, string newNumber)
+        {
+            var oldPhone = Phones.FirstOrDefault(p => p.LocalCode == oldLocalCode && p.Number == oldNumber); ;
+
+            if (oldPhone != null)
+                Phones.Remove(oldPhone);
+
+            if (!Phones.Any(p => p.LocalCode == newLocalCode && p.Number == newNumber))
+                Phones.Add(new Phone(newLocalCode, newNumber));
+        }
+    }
+}
