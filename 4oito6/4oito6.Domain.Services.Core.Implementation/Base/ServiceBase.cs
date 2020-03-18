e7369b97fb70527bc33972c25c114b32b9417fa9
Rennan Rezende <rennan765@gmail.com>
2020-03-18 18:55:41 -0300

    Inserido tipo genérico na spec.

--- 4oito6/4oito6.Domain.Services.Core.Implementation/Base/ServiceBase.cs
+++ 4oito6/4oito6.Domain.Services.Core.Implementation/Base/ServiceBase.cs
@@ -1,4 +1,5 @@
-﻿using _4oito6.Domain.Services.Core.Contracts.Interfaces;
+﻿using _4oito6.Domain.Model.Core.Entities;
+using _4oito6.Domain.Services.Core.Contracts.Interfaces;
 using _4oito6.Domain.Specs.Core.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,13 +10,13 @@ namespace _4oito6.Domain.Services.Core.Implementation.Base
     public abstract class ServiceBase : IServiceBase
     {
         private bool _disposedValue;
-        private IList<IBusinessSpec> _businessSpecs;
+        private IList<IBusinessSpec<EntityBase>> _businessSpecs;
         private IDisposable[] _repositories;
 
         public ServiceBase(IDisposable[] repositories)
         {
             _repositories = repositories;
-            _businessSpecs = new List<IBusinessSpec>();
+            _businessSpecs = new List<IBusinessSpec<EntityBase>>();
             _disposedValue = false;
         }
 
@@ -47,7 +48,7 @@ namespace _4oito6.Domain.Services.Core.Implementation.Base
             GC.SuppressFinalize(this);
         }
 
-        public virtual void AddSpec(IBusinessSpec businessSpec)
+        public virtual void AddSpec(IBusinessSpec<EntityBase> businessSpec)
         {
             _businessSpecs.Add(businessSpec);
         }
