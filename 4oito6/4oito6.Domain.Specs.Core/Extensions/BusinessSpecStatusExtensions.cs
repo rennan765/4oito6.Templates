6778b62a2448048bfed7378209707fe93b593e90
Rennan Rezende <rennan765@gmail.com>
2020-03-18 19:44:55 -0300

    Inserida nova regra no IsSatisfied do BusinessSpec.

---
+++ 4oito6/4oito6.Domain.Specs.Core/Extensions/BusinessSpecStatusExtensions.cs
@@ -0,0 +1,36 @@
+﻿using _4oito6.Domain.Specs.Core.Enum;
+
+namespace _4oito6.Domain.Specs.Core.Extensions
+{
+    public static class BusinessSpecStatusExtensions
+    {
+        public static int FirstCodeNumber(this BusinessSpecStatus status)
+        {
+            switch (status)
+            {
+                case BusinessSpecStatus.Success:
+                case BusinessSpecStatus.Created:
+                case BusinessSpecStatus.Accepted:
+                case BusinessSpecStatus.NoContent:
+                    return 2;
+
+                case BusinessSpecStatus.InvalidInputs:
+                case BusinessSpecStatus.Unauthorized:
+                case BusinessSpecStatus.PaymentRequired:
+                case BusinessSpecStatus.Forbidden:
+                case BusinessSpecStatus.ResourceNotFound:
+                case BusinessSpecStatus.Conflict:
+                case BusinessSpecStatus.Locked:
+                    return 4;
+
+                case BusinessSpecStatus.InternalError:
+                case BusinessSpecStatus.Badgateway:
+                case BusinessSpecStatus.ServiceUnavailable:
+                    return 5;
+
+                default:
+                    return 5;
+            }
+        }
+    }
+}
