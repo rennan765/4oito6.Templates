be4a18dafcad4f13b8efd8a47dc61391410f19bd
Rennan Rezende <rennan765@gmail.com>
2020-03-18 18:40:27 -0300

    UserRepository.GetByIdAsync sobrecarregado. Alterado nome da solution folder para Reporitory.

--- 4oito6/4oito6.Template.Infra.Data.Repositories.Implementation/Entity/UserRepository.cs
+++ 4oito6/4oito6.Template.Infra.Data.Repositories.Implementation/Entity/UserRepository.cs
@@ -2,6 +2,8 @@
 using _4oito6.Template.Infra.Data.Context;
 using _4oito6.Template.Infra.Data.Model.Entities;
 using _4oito6.Template.Infra.Data.Repositories.Contracts.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace _4oito6.Template.Infra.Data.Repositories.Implementation.Entity
 {
@@ -9,6 +11,15 @@ namespace _4oito6.Template.Infra.Data.Repositories.Implementation.Entity
     {
         public UserRepository(TemplateContext context) : base(context)
         {
+        }
+
+        public override async Task<User> GetByIdAsync(int id)
+        {
+            return await Context.Set<User>()
+                .Include(u => u.Address)
+                .Include(u => u.Phones).ThenInclude(up => up.Phone)
+                .FirstOrDefaultAsync(u => u.Id == id)
+                .ConfigureAwait(false);
         }
     }
 }
