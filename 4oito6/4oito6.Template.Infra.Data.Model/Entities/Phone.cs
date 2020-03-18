8c05233090509087b723980ae29781bb7c1d0a9e
Rennan Rezende <rennan765@gmail.com>
2020-03-18 18:09:15 -0300

    Entidades com sets privados. Criados construtpres para as entidades.

--- 4oito6/4oito6.Template.Infra.Data.Model/Entities/Phone.cs
+++ 4oito6/4oito6.Template.Infra.Data.Model/Entities/Phone.cs
@@ -5,15 +5,23 @@ namespace _4oito6.Template.Infra.Data.Model.Entities
 {
     public class Phone : DataModelBase
     {
+        public long Id { get; private set; }
+        public string LocalCode { get; private set; }
+        public string Number { get; private set; }
+
+        public virtual IList<UserPhone> Users { get; private set; }
+
         protected Phone()
         {
             Users = new List<UserPhone>();
         }
 
-        public long Id { get; set; }
-        public string LocalCode { get; set; }
-        public string Number { get; set; }
-
-        public virtual IList<UserPhone> Users { get; set; }
+        public Phone(long id, string localCode, string number, IList<UserPhone> users)
+        {
+            Id = id;
+            LocalCode = localCode;
+            Number = number;
+            Users = users;
+        }
     }
 }
