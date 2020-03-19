deb76e176a296982b220dfb5d174b8fbf23c9022
Rennan Rezende <rennan765@gmail.com>
2020-03-19 09:29:09 -0300

    Criação do mapeamento de hpne e address. Refatoração do mapeamento de user, usando os novos mapeamentos criados.

---
+++ 4oito6/4oito6.Template.Infra.Data.Bus.Contracts/Mapper/AddressMapper.cs
@@ -0,0 +1,14 @@
+﻿using DataModel = _4oito6.Template.Infra.Data.Model.Entities;
+using DomainModel = _4oito6.Template.Domain.Model.Entities;
+
+namespace _4oito6.Template.Infra.Data.Bus.Contracts.Mapper
+{
+    public static class AddressMapper
+    {
+        public static DomainModel.Address ToDomainModel(this DataModel.Address address)
+            => new DomainModel.Address(address.Id, address.Street, address.Number, address.Complement, address.District, address.City, address.State, address.PostalCode);
+
+        public static DataModel.Address ToDomainModel(this DomainModel.Address address)
+            => new DataModel.Address(address.Id, address.Street, address.Number, address.Complement, address.District, address.City, address.State, address.PostalCode);
+    }
+}
---
+++ 4oito6/4oito6.Template.Infra.Data.Bus.Contracts/Mapper/PhoneMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataModel = _4oito6.Template.Infra.Data.Model.Entities;
+using DomainModel = _4oito6.Template.Domain.Model.Entities;
+
+namespace _4oito6.Template.Infra.Data.Bus.Contracts.Mapper
+{
+    public static class PhoneMapper
+    {
+        public static DomainModel.Phone ToDomainModel(this DataModel.Phone phone)
+            => new DomainModel.Phone(phone.Id, phone.LocalCode, phone.Number);
+
+        public static DomainModel.Phone ToDomainModel(this DataModel.UserPhone phone)
+            => new DomainModel.Phone(phone.IdPhone, phone.Phone?.LocalCode, phone.Phone?.Number);
+
+        public static IList<DataModel.UserPhone> ToDataModel(this IList<DomainModel.Phone> phones, DomainModel.User user)
+            => phones.Select(p => new DataModel.UserPhone(user.Id, p.Id)).ToList();
+    }
+}
