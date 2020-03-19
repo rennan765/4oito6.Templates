c53277db55df55f988e1e0dfaf1f242a695e9e12
Rennan Rezende <rennan765@gmail.com>
2020-03-19 11:43:37 -0300

    Criação do ResponseMessage.

---
+++ 4oito6/4oito6.Domain.Application.Core.Contracts/Arguments/ResponseMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace _4oito6.Domain.Application.Core.Contracts.Arguments
+{
+    public class ResponseMessage<TResponse>
+        where TResponse : class
+    {
+        public TResponse Data { get; set; }
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public int? TotalRows { get; set; }
+        public int StatusCode { get; set; }
+
+        public ResponseMessage()
+        {
+        }
+
+        public ResponseMessage(TResponse data, string message, string[] errors, int? totalRows, int statusCode)
+        {
+            Data = data;
+            Message = message;
+            Errors = errors;
+            TotalRows = totalRows;
+            StatusCode = statusCode;
+        }
+    }
+}
