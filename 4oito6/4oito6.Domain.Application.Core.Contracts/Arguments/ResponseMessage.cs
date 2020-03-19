cf38867f1793fac7f4d93dfafeea08e0147cfc6e
Rennan Rezende <rennan765@gmail.com>
2020-03-19 11:56:11 -0300

    Incluído ResponseMessage no middleware.

--- 4oito6/4oito6.Domain.Application.Core.Contracts/Arguments/ResponseMessage.cs
+++ 4oito6/4oito6.Domain.Application.Core.Contracts/Arguments/ResponseMessage.cs
@@ -1,9 +1,7 @@
 ﻿namespace _4oito6.Domain.Application.Core.Contracts.Arguments
 {
-    public class ResponseMessage<TResponse>
-        where TResponse : class
+    public class ResponseMessage
     {
-        public TResponse Data { get; set; }
         public string Message { get; set; }
         public string[] Errors { get; set; }
         public int? TotalRows { get; set; }
@@ -13,13 +11,67 @@
         {
         }
 
-        public ResponseMessage(TResponse data, string message, string[] errors, int? totalRows, int statusCode)
+        public ResponseMessage(string message, int? totalRows, int statusCode)
         {
-            Data = data;
+            Message = message;
+            TotalRows = totalRows;
+            StatusCode = statusCode;
+        }
+
+        public ResponseMessage(string message, string[] errors, int? totalRows, int statusCode)
+        {
             Message = message;
             Errors = errors;
             TotalRows = totalRows;
             StatusCode = statusCode;
+        }
+
+        public ResponseMessage(string message, string[] errors, int statusCode)
+        {
+            Message = message;
+            Errors = errors;
+            StatusCode = statusCode;
+        }
+
+        public ResponseMessage(string message, int statusCode)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+    }
+
+    public class ResponseMessage<TResponse> : ResponseMessage
+        where TResponse : class
+    {
+        public TResponse Data { get; set; }
+
+        public ResponseMessage()
+            : base()
+        {
+        }
+
+        public ResponseMessage(TResponse data, string message, int? totalRows, int statusCode)
+            : base(message, totalRows, statusCode)
+        {
+            Data = data;
+        }
+
+        public ResponseMessage(TResponse data, string message, string[] errors, int? totalRows, int statusCode)
+            : base(message, errors, totalRows, statusCode)
+        {
+            Data = data;
+        }
+
+        public ResponseMessage(TResponse data, string message, string[] errors, int statusCode)
+            : base(message, errors, statusCode)
+        {
+            Data = data;
+        }
+
+        public ResponseMessage(TResponse data, string message, int statusCode)
+            : base(message, statusCode)
+        {
+            Data = data;
         }
     }
 }
