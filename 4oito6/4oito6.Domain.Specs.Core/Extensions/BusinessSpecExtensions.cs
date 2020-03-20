623e1d4f57b3a6a0c1bb52a63d054b18b9b67727
Rennan Rezende <rennan765@gmail.com>
2020-03-20 12:38:44 -0300

    Criado método para retornar o status code de um spec ou uma lista de specs.

---
+++ 4oito6/4oito6.Domain.Specs.Core/Extensions/BusinessSpecExtensions.cs
@@ -0,0 +1,17 @@
+﻿using _4oito6.Domain.Specs.Core.Enum;
+using _4oito6.Domain.Specs.Core.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace _4oito6.Domain.Specs.Core.Extensions
+{
+    public static class BusinessSpecExtensions
+    {
+        public static BusinessSpecStatus ToBusinessSpecStatus(this IBusinessSpec spec)
+            => spec.Messages.Select(m => m.Status).ToList().ToMajorStatus();
+
+        public static HttpStatusCode ToHttpStatusCode(this IList<IBusinessSpec> specs)
+            => specs.Select(s => s.ToBusinessSpecStatus()).ToList().ToMajorStatus().ToHttpStatusCode();
+    }
+}
