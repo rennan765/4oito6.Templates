c5c3af11eebae700ceccbbc69b983eefb9469f03
Rennan Rezende <rennan765@gmail.com>
2020-03-20 13:25:01 -0300

    Início do IoC.

---
+++ 4oito6/4oito6.Template.Infra.CrossCutting.Ioc/IoC.cs
@@ -0,0 +1,20 @@
+﻿using _4oito6.Infra.CrossCutting.Configuration.Connection;
+using _4oito6.Template.Infra.CrossCutting.Ioc.Resolvers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _4oito6.Template.Infra.CrossCutting.Ioc
+{
+    public static class IoC
+    {
+        public static IServiceCollection ResolveTemplate(this IServiceCollection services)
+        {
+            return services
+                .AddSingleton<IConnectionConfiguration, ConnectionConfiguration>()
+                .ResolveDatabase()
+                .ResolveRepositories()
+                .ResolveBus()
+                .ResolveServices()
+                .ResolveAppServices();
+        }
+    }
+}
---
+++ 4oito6/4oito6.Template.Infra.CrossCutting.Ioc/Resolvers/DatabaseResolver.cs
@@ -0,0 +1,29 @@
+﻿using _4oito6.Infra.CrossCutting.Configuration.Connection;
+using _4oito6.Infra.Data.Core.Connection;
+using _4oito6.Template.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace _4oito6.Template.Infra.CrossCutting.Ioc.Resolvers
+{
+    public static class DatabaseResolver
+    {
+        public static IServiceCollection ResolveDatabase(this IServiceCollection services)
+        {
+            IConnectionConfiguration config = null;
+
+            return services
+                .AddScoped<IAsyncDbConnection>
+                (
+                    (sp) =>
+                    {
+                        config = sp.GetService<IConnectionConfiguration>();
+
+                        return new AsyncDbConnection(new NpgsqlConnection(config.DbConnectionString));
+                    }
+                )
+                .AddDbContext<TemplateContext>(options => options.UseNpgsql(config.DbConnectionString));
+        }
+    }
+}
