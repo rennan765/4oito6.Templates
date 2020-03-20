c235360d2fce1239013cc0b49ec77468a4322920
Rennan Rezende <rennan765@gmail.com>
2020-03-20 12:22:22 -0300

    Incluído método para pegar o status code de uma spec.

--- 4oito6/4oito6.Domain.Specs.Core/Extensions/BusinessSpecStatusExtensions.cs
+++ 4oito6/4oito6.Domain.Specs.Core/Extensions/BusinessSpecStatusExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using _4oito6.Domain.Specs.Core.Enum;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace _4oito6.Domain.Specs.Core.Extensions
 {
@@ -31,6 +34,44 @@ namespace _4oito6.Domain.Specs.Core.Extensions
                 default:
                     return 5;
             }
+        }
+
+        public static HttpStatusCode ToHttpStatusCode(this BusinessSpecStatus status)
+            => (HttpStatusCode)status;
+
+        public static int ToInt(this BusinessSpecStatus status) => (int)status;
+
+        public static BusinessSpecStatus ToBusinessSpecStatus(this int status) => (BusinessSpecStatus)status;
+
+        public static BusinessSpecStatus ToMajorStatus(this IList<BusinessSpecStatus> status)
+        {
+            if (status.Any(m => m.FirstCodeNumber() == 5))
+            {
+                if (status.Any(m => m == BusinessSpecStatus.ServiceUnavailable))
+                    return BusinessSpecStatus.ServiceUnavailable;
+                if (status.Any(m => m == BusinessSpecStatus.Badgateway))
+                    return BusinessSpecStatus.Badgateway;
+
+                return BusinessSpecStatus.InternalError;
+            }
+
+            if (status.Any(m => m.FirstCodeNumber() == 4))
+            {
+                if (status.Any(m => m == BusinessSpecStatus.ResourceNotFound))
+                    return BusinessSpecStatus.ResourceNotFound;
+
+                if (status.Any(m => m == BusinessSpecStatus.PaymentRequired))
+                    return BusinessSpecStatus.PaymentRequired;
+
+                return status
+                    .Where(m => m != BusinessSpecStatus.ResourceNotFound && m != BusinessSpecStatus.PaymentRequired)
+                    .Select(m => m).Max(m => m.ToInt()).ToBusinessSpecStatus();
+            }
+
+            if (status.Any(m => m == BusinessSpecStatus.Success))
+                return BusinessSpecStatus.Success;
+
+            return status.Select(m => m).Max(m => m.ToInt()).ToBusinessSpecStatus();
         }
     }
 }
---
+++ 4oito6/4oito6.Infra.CrossCutting.Extensions/IntExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace _4oito6.Infra.CrossCutting.Extensions
+{
+    public static class IntExtensions
+    {
+        public static HttpStatusCode ToHttpStatusCode(this int i) => (HttpStatusCode)i;
+    }
+}
