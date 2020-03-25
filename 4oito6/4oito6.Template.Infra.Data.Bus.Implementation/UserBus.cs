23f7a84af7fd3f71faa085590a384980395ae1a4
Rennan Rezende <rennan765@gmail.com>
2020-03-25 15:07:18 -0300

    UserBus.UpdateUserAsync

--- 4oito6/4oito6.Template.Infra.Data.Bus.Implementation/UserBus.cs
+++ 4oito6/4oito6.Template.Infra.Data.Bus.Implementation/UserBus.cs
@@ -4,6 +4,7 @@ using _4oito6.Template.Infra.Data.Bus.Contracts.Interfaces;
 using _4oito6.Template.Infra.Data.Bus.Contracts.Mapper;
 using _4oito6.Template.Infra.Data.Repositories.Contracts.Entity;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DomainModel = _4oito6.Template.Domain.Model.Entities;
 
@@ -43,6 +44,35 @@ namespace _4oito6.Template.Infra.Data.Bus.Implementation
 
         public async Task<DomainModel.User> UpdateUserAsync(DomainModel.User user)
         {
+            var userDb = await _userRepository.GetByIdAsync(user.Id).ConfigureAwait(false);
+
+            if (user.Address != null)
+            {
+                if (userDb.IdAddress != user.Address.Id)
+                {
+                    userDb.IdAddress = user.Address.Id > 0 ? user.Address.Id : userDb.IdAddress;
+                    userDb.Address = user.Address.ToDataModel();
+                }
+            }
+            else
+            {
+                userDb.IdAddress = null;
+                userDb.Address = null;
+            }
+
+            //Remove who is in db and is not on screen
+            userDb.Phones.ToList().ForEach(up =>
+            {
+                if (!user.Phones.Where(p => p.Id > 0).Any(p => p.Id == up.IdPhone))
+                    userDb.Phones.Remove(up);
+            });
+
+            //Add who is on screen and is not in db
+            user.Phones
+                .Where(phone => phone.Id <= 0 || !userDb.Phones.Any(up => up.IdPhone == phone.Id))
+                .ToList().ToDataModel(user).ToList()
+                .ForEach(up => userDb.Phones.Add(up));
+
             var updatedUser = await _userRepository.UpdateAsync(user.ToDataModel()).ConfigureAwait(false);
 
             await Unit.SaveEntityChangesAsync().ConfigureAwait(false);
