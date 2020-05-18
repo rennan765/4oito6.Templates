44d874702324d65921eeb99c26a251f5aee8f023
Rennan Rezende <rennan765@gmail.com>
2020-05-18 17:18:11 -0300

    Projetos core de redis

---
+++ 4oito6/4oito6.Infra.Data.Cache.Core.Contracts/ICacheRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _4oito6.Infra.Data.Cache.Core.Contracts
+{
+    public interface ICacheRepository : IDisposable
+    {
+        Task GetAsync(string key);
+
+        Task RemoveAsync(string key);
+
+        Task SetAsync(string key, string value);
+    }
+}
