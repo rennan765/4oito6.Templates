175a7d20c9511291e2bcf64128775b3f58130616
Rennan Rezende <rennan765@gmail.com>
2020-05-18 23:31:02 -0300

    Início desenvolvimento ws-pcode

---
+++ 4oito6/4oito6.Contact.Infra.CrossCutting.PostalCode/Contracts/Arguments/AddressFromPostalCodeResponse.cs
@@ -0,0 +1,22 @@
+﻿using _4oito6.Contact.Infra.CrossCutting.PostalCode.Model;
+
+namespace _4oito6.Contact.Infra.CrossCutting.PostalCode.Contracts.Arguments
+{
+    public class AddressFromPostalCodeResponse
+    {
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public static explicit operator AddressFromPostalCodeResponse(AddressFromPostalCode address)
+            => new AddressFromPostalCodeResponse
+            {
+                Street = address.Street,
+                District = address.District,
+
+                City = address.City,
+                State = address.State
+            };
+    }
+}
---
+++ 4oito6/4oito6.Contact.Infra.CrossCutting.PostalCode/Model/AddressFromPostalCode.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace _4oito6.Contact.Infra.CrossCutting.PostalCode.Model
+{
+    public class AddressFromPostalCode
+    {
+        [JsonProperty(PropertyName = "cep")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty(PropertyName = "logradouro")]
+        public string Street { get; set; }
+
+        [JsonProperty(PropertyName = "complemento")]
+        public string Complement { get; set; }
+
+        [JsonProperty(PropertyName = "bairro")]
+        public string District { get; set; }
+
+        [JsonProperty(PropertyName = "localidade")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "uf")]
+        public string State { get; set; }
+
+        [JsonProperty(PropertyName = "unidade")]
+        public string Unity { get; set; }
+
+        [JsonProperty(PropertyName = "ibge")]
+        public string Ibge { get; set; }
+
+        [JsonProperty(PropertyName = "gia")]
+        public string Gia { get; set; }
+    }
+}
