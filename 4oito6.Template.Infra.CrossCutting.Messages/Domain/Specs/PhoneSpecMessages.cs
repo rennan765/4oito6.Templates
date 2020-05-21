7cb5f26f105eb622bf461353a27c01efb6fbf5c2
Rennan Rezende <rennan765@gmail.com>
2020-05-21 11:34:52 -0300

    Template messages

---
+++ 4oito6.Template.Infra.CrossCutting.Messages/Domain/Specs/PhoneSpecMessages.cs
@@ -0,0 +1,20 @@
+﻿namespace _4oito6.Template.Infra.CrossCutting.Messages.Domain.Specs
+{
+    public static class PhoneSpecMessages
+    {
+        public static string DddObrigatorio { get; private set; }
+        public static string DddInvalido { get; private set; }
+
+        public static string NumeroObrigatorio { get; private set; }
+        public static string NumeroInvalido { get; private set; }
+
+        static PhoneSpecMessages()
+        {
+            DddObrigatorio = "O DDD é obrigatório.";
+            DddInvalido = "O DDD precisa ter 2 caracteres.";
+
+            NumeroObrigatorio = "O número é obrigatório.";
+            NumeroInvalido = "O DDD precisa ter 8 ou 9 caracteres.";
+        }
+    }
+}
