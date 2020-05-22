64cd7972d8795546f3f2694762a374091a4b0386
Rennan Rezende <rennan765@gmail.com>
2020-05-22 18:15:59 -0300

    bson elements

--- 4oito6.AuditTrail.Infra.Mongo.Repositories.Contracts/Model/AuditTrailDto.cs
+++ 4oito6.AuditTrail.Infra.Mongo.Repositories.Contracts/Model/AuditTrailDto.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace _4oito6.AuditTrail.Infra.Mongo.Repositories.Contracts.Model
 {
     public sealed class AuditTrailDto
     {
+        [BsonElement("_id")]
         public string Id { get; set; }
+
+        [BsonElement("date")]
         public DateTime Date { get; set; }
+
+        [BsonElement("message")]
         public string Message { get; set; }
+
+        [BsonElement("stackTrace")]
         public string StackTrace { get; set; }
     }
 }
