a7fa2a2f2808199ca5fc3662d65005b9c77f7487
Rennan Rezende <rennan765@gmail.com>
2020-05-22 15:52:55 -0300

    MongoRepositoryBase

---
+++ 4oito6.Infra.Data.Repositories.Core.Contracts/IMongoRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _4oito6.Infra.Data.Repositories.Core.Contracts
+{
+    public interface IMongoRepositoryBase<TEntity>
+        where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        Task<IList<TEntity>> GetAsync();
+    }
+}
