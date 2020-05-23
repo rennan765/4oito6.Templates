a7639041f0f3a89f6123dd6bbdfbd68e1b314b31
Rennan Rezende <rennan765@gmail.com>
2020-05-23 14:21:28 -0300

    Ajuste no middleware para inserir na trilha

--- 4oito6.Template.Api/Middleware/ExceptionHandlerMiddleware.cs
+++ 4oito6.Template.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -1,12 +1,31 @@
-﻿using _4oito6.Middleware.Core;
+﻿using _4oito6.AuditTrail.Application.Contracts;
+using _4oito6.Middleware.Core;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
 
 namespace _4oito6.Template.Api.Middleware
 {
     public class ExceptionHandlerMiddleware : MidldlewareBase
     {
-        public ExceptionHandlerMiddleware(RequestDelegate next) : base(next)
+        private readonly IAuditTrailAppService _auditTrailAppService;
+
+        public ExceptionHandlerMiddleware(IAuditTrailAppService auditTrailAppService, RequestDelegate next) : base(next)
         {
+            _auditTrailAppService = auditTrailAppService ?? throw new ArgumentNullException(nameof(auditTrailAppService));
+        }
+
+        public override async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await Next(context).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                await TreatExceptionAsync(context, ex).ConfigureAwait(false);
+                await _auditTrailAppService.CreateAsync(ex).ConfigureAwait(false);
+            }
         }
     }
 }
